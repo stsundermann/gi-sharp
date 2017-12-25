4d7caef514cd36559271ad582a9cfddb1ecfd4b3
Stephan Sundermann <stephansundermann@gmail.com>
2017-12-25 12:07:36 +0100

    Implement interfaces

--- generator/Gir/Interface.cs
+++ generator/Gir/Interface.cs
@@ -1,6 +1,51 @@
-﻿namespace Generator.Gir
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Generator.Gir
 {
 	public class Interface
 	{
+        [XmlAttribute("name")]
+        public string Name;
+
+        [XmlAttribute("version")]
+        public string Version;
+
+        [XmlAttribute("symbol-prefix", Namespace = "http://www.gtk.org/introspection/c/1.0")]
+        public string CSymbolPrefix;
+
+        [XmlAttribute("type", Namespace = "http://www.gtk.org/introspection/c/1.0")]
+        public string CType;
+
+        [XmlAttribute("get-type", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public string GLibGetType;
+
+        [XmlAttribute("type-name", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public string GLibTypeName;
+
+        [XmlAttribute("type-struct", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public string GLibTypeStruct;
+
+        [XmlElement("doc")]
+        public string Doc;
+
+        [XmlElement("prerequisite")]
+        public List<Prerequisite> Prerequisites;
+
+        [XmlElement("property")]
+        public List<Property> Properties;
+
+        [XmlElement("method")]
+        public List<Method> Methods;
+
+        [XmlElement("virtual-method")]
+        public List<VirtualMethod> VirtualMethods;
+
+        [XmlElement("function")]
+        public List<Function> Functions;
+
+        [XmlElement("signal", Namespace = "http://www.gtk.org/introspection/glib/1.0")]
+        public List<Signal> Signals;
+
 	}
 }
