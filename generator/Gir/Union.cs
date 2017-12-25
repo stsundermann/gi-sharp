32161bf4d9804af594848b0f69030214bf1ac920
Stephan Sundermann <stephansundermann@gmail.com>
2017-12-25 14:17:16 +0100

    Implement union

--- generator/Gir/Union.cs
+++ generator/Gir/Union.cs
@@ -1,6 +1,26 @@
-﻿namespace Generator.Gir
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Generator.Gir
 {
 	public class Union
 	{
+        [XmlAttribute("name")]
+        public string Name;
+
+        [XmlAttribute("type", Namespace = "http://www.gtk.org/introspection/c/1.0")]
+        public string CType;
+
+        [XmlElement("doc")]
+        public string Doc;
+
+        [XmlElement("field")]
+        public List<Field> Fields;
+
+        [XmlElement("method")]
+        public List<Method> Methods;
+
+		[XmlElement("record")]
+		public List<Record> Records;
 	}
 }
